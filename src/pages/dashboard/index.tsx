4d8cb4efed5c45cc8e9747ed5544a95277918b80
Bogdan <bogdasafronov@gmail.com>
2023-10-01 23:03:51 +0300

    feat(head): rename profile title

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,17 +1,25 @@
+import Head from "next/head";
 import { GroupList } from "~/features/group";
 import { Card } from "~/shared/ui/card";
 
 export default function DashboardPage() {
   return (
-    <div className="grid w-full grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-      <GroupList />
-      <div>
-        <h5 className="mb-2 text-3xl font-semibold">Мои приглашения</h5>
-        <Card className="p-4">
-          <div className="mb-2">У вас пока нет приглашений</div>
-          <div className="flex justify-end"></div>
-        </Card>
+    <>
+      <Head>
+        <title>Мои дети. Моя страница</title>
+        <meta name="description" content="Создано с любовью для воспитателей" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="grid w-full grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        <GroupList />
+        <div>
+          <h5 className="mb-2 text-3xl font-semibold">Мои приглашения</h5>
+          <Card className="p-4">
+            <div className="mb-2">У вас пока нет приглашений</div>
+            <div className="flex justify-end"></div>
+          </Card>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
