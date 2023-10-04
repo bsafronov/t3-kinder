da8f8d699ec1dce68b618fd10a0424f5153b3166
Bogdan Safronov <bogdasafronov@gmail.com>
2023-10-04 17:50:23 +0300

    feat(api): absence & tag

--- src/server/api/routers/absence-tag.ts
+++ src/server/api/routers/absence-tag.ts
@@ -1,3 +1,59 @@
-import { createTRPCRouter } from "../trpc";
+import { z } from "zod";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
 
-export const absenceTagRouter = createTRPCRouter({});
+export const absenceTagRouter = createTRPCRouter({
+  create: protectedProcedure
+    .input(
+      z.object({
+        groupId: z.string(),
+        label: z.string(),
+      }),
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.db.absenceTag.create({
+        data: {
+          groupId: input.groupId,
+          label: input.label,
+        },
+      });
+    }),
+  update: protectedProcedure
+    .input(
+      z.object({
+        absenceTagId: z.string(),
+        label: z.string(),
+      }),
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.db.absenceTag.update({
+        where: {
+          id: input.absenceTagId,
+        },
+        data: {
+          label: input.absenceTagId,
+        },
+      });
+    }),
+  getOne: protectedProcedure
+    .input(
+      z.object({
+        absenceTagId: z.string(),
+      }),
+    )
+    .query(({ ctx, input }) => {
+      return ctx.db.absenceTag.findUnique({
+        where: {
+          id: input.absenceTagId,
+        },
+      });
+    }),
+  getManyByGroup: protectedProcedure
+    .input(z.object({ groupId: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.db.absenceTag.findMany({
+        where: {
+          groupId: input.groupId,
+        },
+      });
+    }),
+});
--- src/server/api/routers/absence.ts
+++ src/server/api/routers/absence.ts
@@ -1,3 +1,81 @@
-import { createTRPCRouter } from "../trpc";
+import { z } from "zod";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
 
-export const absenceRouter = createTRPCRouter({});
+export const absenceRouter = createTRPCRouter({
+  create: protectedProcedure
+    .input(
+      z.object({
+        groupId: z.string(),
+        kidId: z.string(),
+        date: z.string(),
+        reason: z.string(),
+        tagIDs: z.array(z.string()),
+      }),
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.db.absence.create({
+        data: {
+          createdById: ctx.session.user.id,
+          groupId: input.groupId,
+          kidId: input.kidId,
+          date: input.date,
+          reason: input.reason,
+          tagIDs: input.tagIDs,
+        },
+      });
+    }),
+  update: protectedProcedure
+    .input(
+      z.object({
+        absenceId: z.string(),
+        date: z.string(),
+        reason: z.string(),
+        tagIDs: z.array(z.string()),
+      }),
+    )
+    .mutation(({ ctx, input }) => {
+      return ctx.db.absence.update({
+        where: {
+          id: input.absenceId,
+        },
+        data: {
+          updatedById: ctx.session.user.id,
+          date: input.date,
+          reason: input.reason,
+          tagIDs: input.tagIDs,
+        },
+      });
+    }),
+  getOne: protectedProcedure
+    .input(
+      z.object({
+        absenceId: z.string(),
+      }),
+    )
+    .query(({ ctx, input }) => {
+      return ctx.db.absence.findUnique({
+        where: {
+          id: input.absenceId,
+        },
+        include: {
+          createdBy: true,
+          updatedBy: true,
+          tags: true,
+        },
+      });
+    }),
+  getManyByKid: protectedProcedure
+    .input(z.object({ groupId: z.string() }))
+    .query(({ ctx, input }) => {
+      return ctx.db.absence.findMany({
+        where: {
+          groupId: input.groupId,
+        },
+        include: {
+          createdBy: true,
+          updatedBy: true,
+          tags: true,
+        },
+      });
+    }),
+});
