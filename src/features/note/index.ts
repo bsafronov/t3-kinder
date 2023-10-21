03a8e360af64faaac5e960e76e22b9c4639c3f07
Bogdan <bogdasafronov@gmail.com>
2023-10-21 18:11:46 +0300

    fix(ui): delete table

--- src/features/note/index.ts
+++ src/features/note/index.ts
@@ -3,5 +3,4 @@ export * as noteAPI from "./api";
 export { NoteCreateModal } from "./components/create-modal";
 export { NoteUpdateModal } from "./components/update-modal";
 export { NoteItem } from "./components/item";
-export { NoteTable } from "./components/table/index";
 export { NoteGroupScreen } from "./components/group-screen";
--- src/features/parent/index.ts
+++ src/features/parent/index.ts
@@ -4,5 +4,4 @@ export { ParentItem } from "./components/item";
 export { ParentEditModal } from "./components/edit-modal";
 export { ParentCreateModal } from "./components/create-modal";
 export { ParentSelectModal } from "./components/select-modal";
-export { ParentTable } from "./components/table/index";
 export { ParentGroupScreen } from "./components/group-screen";
